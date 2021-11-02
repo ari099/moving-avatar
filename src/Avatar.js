99d6185bd00dba1872db8fd41c89e369ee9f1fce
ari099 <ariellorenzo08@gmail.com>
2021-11-02 08:53:26 -0400

    Changed head and glasses colors

--- src/Avatar.js
+++ src/Avatar.js
@@ -33,7 +33,7 @@ const Avatar = props => {
                transform: [surpriseGlassesCoords, initialGlassesCoords],
           }, '-=400').add({
                targets: ".Avatar-glasses-top",
-               fill: ['#97b5e2', '#000', '#97b5e2']
+               fill: ['#97b5e2', '#164750', '#97b5e2']
           });
      };
 
